a96395c9ba553c3358c1dbe540c8b46c79853e65
John-@Gallimore <john@gallimoresoftware.com>
2024-10-03 04:34:03 +0800

    NFG-86 'Fixed errors in loggerConfig'

--- src/infrastructure/logging/loggerConfig.ts
+++ src/infrastructure/logging/loggerConfig.ts
@@ -1,4 +1,4 @@
-import { format, transports } from "winston";
+import { format, transports, createLogger } from "winston";
 import "winston-mongodb";
 import dotenv from "dotenv";
 
@@ -6,25 +6,23 @@ dotenv.config();
 
 const logLevel = process.env.LOG_LEVEL || "info";
 
-const loggerConfig = {
+// Create the logger configuration
+const loggerConfig = createLogger({
   level: logLevel,
   format: format.combine(format.timestamp(), format.json()),
   transports: [
     new transports.Console({
-      format: format.combine(
-        format.colorize(), // Add colorize for better readability in the console
-        format.simple(),
-      ),
+      format: format.combine(format.colorize(), format.simple()),
       level: logLevel, // The console transport respects the global log level
     }),
   ],
-};
+});
 
 // Add MongoDB transport if a connection string is provided
 if (process.env.MONGODB_URI) {
-  loggerConfig.transports.push(
+  loggerConfig.add(
     new transports.MongoDB({
-      level: "error", // Log errors to MongoDB by default
+      level: "error",
       db: process.env.MONGODB_URI,
       options: { useUnifiedTopology: true },
       collection: "app_logs",
