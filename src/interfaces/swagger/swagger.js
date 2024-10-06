29b5b8083cbb5d0cfc44b2cdc8506696cdaf7562
Nicholas Gallimore <nick@gallimoresoftware.com>
2024-10-05 20:57:35 -0400

    NFG-86 clean js map files

--- src/interfaces/swagger/swagger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const swaggerAutogen = require("swagger-autogen")();
-const outputFile = "./swagger_output.json";
-const endpointsFiles = [
-    "./src/routes/product-development-routes/*.js",
-    "./src/routes/sales-routes/*.js",
-    "*.js",
-];
-swaggerAutogen(outputFile, endpointsFiles);
