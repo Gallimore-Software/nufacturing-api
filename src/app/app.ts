29d821e2bedaef29a68d2a5d5caeafbeacf21633
Nicholas Gallimore <nick@gallimoresoftware.com>
2024-10-07 18:59:18 -0400

    NFG-86 fix app.ts route imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,28 +3,29 @@ import express from "express";
 import swaggerUI from "swagger-ui-express";
 
 // Schedule tasks
-import "@scheduling/scheduledTasks";
+import "@scheduling/scheduled-tasks";
 
 // Importing database connection function
 import connectDB from "@infrastructure/persistence/db";
 
 // Import route handlers
 import inventoryRoutes from "@interfaces/http/routes/inventory/inventory-routes";
-import batchRecordsRoutes from "@interfaces/http/routes/quality-audits/batch-records-routes";
-import receivingRoutes from "@interfaces/http/routes/receiving/receiving-routes";
-import userRoutes from "@interfaces/http/routes/users/user-routes";
-import formulaRoutes from "@interfaces/http/routes/product-development/formula-routes";
-import productSkuRoutes from "@interfaces/http/routes/product-development/product-sku-routes";
-import productTypeRoutes from "@interfaces/http/routes/product-development/product-type-routes";
-import quoteRoutes from "@interfaces/http/routes/sales/quote-routes";
-import orderRoutes from "@interfaces/http/routes/sales/order-routes";
-import customerRoutes from "@interfaces/http/routes/sales/customer-routes";
-import purchaseOrderRoutes from "@interfaces/http/routes/receiving/purchase-orders-routes";
-import labTestingRoutes from "@interfaces/http/routes/research-and-development/lab-test-routes";
-import vendorRoutes from "@interfaces/http/routes/vendors/vendor-routes";
+
 
 // Import Swagger documentation
 import swaggerDocument from "@docs/generated/swagger_output.json";
+import customerRoutes from "@interfaces/http/routes/customer/customer-routes";
+import formulaRoutes from "@interfaces/http/routes/formula/formula-routes";
+import orderRoutes from "@interfaces/http/routes/order/order-routes";
+import productSkuRoutes from "@interfaces/http/routes/product-sku/product-sku-routes";
+import productTypeRoutes from "@interfaces/http/routes/product-type/product-type-routes";
+import purchaseOrderRoutes from "@interfaces/http/routes/purchase-order/purchase-order-routes";
+import quoteRoutes from "@interfaces/http/routes/quote/quote-routes";
+import receivingRoutes from "@interfaces/http/routes/receiving/receiving-routes";
+import userRoutes from "@interfaces/http/routes/user/user-routes";
+import vendorRoutes from "@interfaces/http/routes/vendor/vendor-routes";
+import batchRecordRoutes from "@interfaces/http/routes/batch-record/batch-record-routes";
+import labTestRoutes from "@interfaces/http/routes/lab-test/lab-test-routes";
 
 // Initialize express app
 const app = express();
@@ -44,9 +45,9 @@ app.use("/api/product-development/product-types", productTypeRoutes);
 app.use("/api/sales/quotes", quoteRoutes);
 app.use("/api/sales/orders", orderRoutes);
 app.use("/api/sales/customers", customerRoutes);
-app.use("/api/quality-audits/batch-records", batchRecordsRoutes);
+app.use("/api/quality-audits/batch-records", batchRecordRoutes);
 app.use("/api/receiving/purchase-order", purchaseOrderRoutes);
-app.use("/api/research-and-development/lab-testing", labTestingRoutes);
+app.use("/api/research-and-development/lab-testing", labTestRoutes);
 app.use("/api/receiving/receiving", receivingRoutes);
 app.use("/api/vendor", vendorRoutes);
 app.use("/api/inventory", inventoryRoutes);
