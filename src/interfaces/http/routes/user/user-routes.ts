b71d8ae8d979bd878e52ad3f9aca7067482977a8
Nicholas Gallimore <nick@gallimoresoftware.com>
2024-10-08 15:16:20 -0400

    NFG-86 update user routes

--- src/interfaces/http/routes/user/user-routes.ts
+++ src/interfaces/http/routes/user/user-routes.ts
@@ -1,42 +1,21 @@
-import * as userController from "@interfaces/http/controllers/user/user-controller";
-import roleMiddleware from "@interfaces/http/middleware/role-middleware";
-import express from "express";
+import { Router } from 'express';
+import { UserController } from '@interfaces/http/controllers/user/user-controller';
 
-import { verifyEmail } from "@@controllers/authController";
+const router = Router();
 
-export default function createRouter() {
-  const router = express.Router();
+// Route to create a user
+router.post('/', UserController.createUser);
 
-  // Define routes with role-based access control
-  router.get(
-    "/all-users",
-    roleMiddleware(["admin", "manager"]),
-    userController.getAllUsers,
-  );
+// Route to get all users
+router.get('/', UserController.getAllUsers);
 
-  router.get(
-    "/all-users/:_id",
-    roleMiddleware(["admin", "manager", "user"]),
-    userController.getUserById,
-  );
+// Route to get a user by ID
+router.get('/:id', UserController.getUserById);
 
-  router.post("/create-user", userController.createUser);
+// Route to update a user
+router.put('/:id', UserController.updateUser);
 
-  router.put(
-    "/update-user/:_id",
-    roleMiddleware(["admin", "manager"]),
-    userController.updateUser,
-  );
+// Route to delete a user
+router.delete('/:id', UserController.deleteUser);
 
-  router.delete(
-    "/delete-user/:_id",
-    roleMiddleware(["admin"]),
-    userController.deleteUser,
-  );
-
-  router.get("/verify/:token", verifyEmail);
-
-  router.post("/login", userController.loginUser);
-
-  return router;
-}
+export default router;
