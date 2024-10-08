d98107449be0d65f33c8c735ea4d6aa9ce9b72a5
Nicholas Gallimore <nick@gallimoresoftware.com>
2024-10-08 11:51:34 -0400

    NFG-86 Generate use cases

--- src/interfaces/repositories/user/user-repository-interface.ts
+++
@@ -1,6 +0,0 @@
-// src/domain/interfaces/repositories/user-repository.interface.ts
-import { User } from '@domain/entities/user';
-
-export interface IUserRepository {
-  findById(userId: string): Promise<User | null>;
-}
