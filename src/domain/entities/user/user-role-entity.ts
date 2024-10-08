e1d9eea3e0fa6ec4b1199a858de54a10efae9f2b
Nicholas Gallimore <nick@gallimoresoftware.com>
2024-10-08 13:26:38 -0400

    NFG-86 remove excess user files

--- src/domain/entities/user/user-role-entity.ts
+++
@@ -1,8 +0,0 @@
-// src/domain/entities/user.ts
-export class User {
-  constructor(public id: string, public role: string) {}
-
-  hasRole(requiredRoles: string[]): boolean {
-    return requiredRoles.includes(this.role);
-  }
-}
