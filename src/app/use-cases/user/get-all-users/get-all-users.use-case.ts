b65cbfb1407d3634d0f61a8820cac8ceda6db412
unknown <chris@gallimoresoftware.com>
2024-10-18 20:25:48 +0800

    NFG-86 restore test for get-all-users

--- src/app/use-cases/user/get-all-users/get-all-users.use-case.ts
+++ src/app/use-cases/user/get-all-users/get-all-users.use-case.ts
@@ -1,28 +1,21 @@
-// import { User } from '@domain/entities/user/user-entity';
-// import { IUserRepository } from '@domain/interfaces/repositories/user.repository.interface';
-// import Logger from '@infrastructure/logging/logger';
-// import { injectable, inject } from 'inversify';
+import { User } from '@domain/entities/user/user-entity';
+import { IUserRepository } from '@domain/interfaces/repositories/user.repository.interface';
+import Logger from '@infrastructure/logging/logger';
+import { injectable, inject } from 'inversify';
 
-// @injectable()
-// export class GetAllUsersUseCase {
-//   constructor(
-//     @inject('IUserRepository') private userRepository: IUserRepository
-//   ) {}
+@injectable()
+export class GetAllUsersUseCase {
+  constructor(
+    @inject('IUserRepository') private userRepository: IUserRepository
+  ) {}
 
-//   async execute(): Promise<User[]> {
-//     try {
-//       const users = await this.userRepository.findAll();
-//       return users;
-//     } catch (error: unknown) {
-//       Logger.error(error);
-//       return [];
-//     }
-//   }
-// }
-test.skip('should return success', () => {
-  // Empty or not yet implemented
-});
-
-test.skip('should handle error', () => {
-  // Empty or not yet implemented
-});
+  async execute(): Promise<User[]> {
+    try {
+      const users = await this.userRepository.findAll();
+      return users;
+    } catch (error: unknown) {
+      Logger.error(error);
+      return [];
+    }
+  }
+}
